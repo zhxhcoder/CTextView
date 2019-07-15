9ab4f09a660194d54a91ec875c234c34ff22ca3a
zhxh <zhxhcoder@gmail.com>
2019-07-15 11:28:53 +0800

    [Version] 1.2 测试

--- app/src/main/java/com/creditease/ctextview/demo/MainActivity.kt
+++ app/src/main/java/com/creditease/ctextview/demo/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.creditease.ctextview.demo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ctvTest.setOnClickListener {
+            Log.d("XXXXX", "时间-" + System.currentTimeMillis())
+        }
     }
 }
